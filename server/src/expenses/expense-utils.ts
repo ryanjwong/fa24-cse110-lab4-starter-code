65a142943344ccadf5b331db071121cfb6b42f15
ryanjwong <ryanjwong007@gmail.com>
2024-11-07 16:26:08 -0800

    db changes

--- server/src/expenses/expense-utils.ts
+++ server/src/expenses/expense-utils.ts
@@ -1,40 +1,54 @@
+import { Database } from "sqlite";
 import { Expense } from "../types";
 import { Request, Response } from "express";
 
-export function createExpenseServer(req: Request, res: Response, expenses: Expense[]) {
-    const { id, cost, description } = req.body;
+export async function createExpenseServer(req: Request, res: Response, db: Database) {
 
-    if (!description || !id || !cost) {
-        return res.status(400).send({ error: "Missing required fields" });
-    }
-
-    const newExpense: Expense = {
-        id: id,
-        description,
-        cost,
+    try {
+        // Type casting the request body to the expected format.
+        const { id, cost, description } = req.body as { id: string, cost: number, description: string };
+ 
+        if (!description || !id || !cost) {
+            return res.status(400).send({ error: "Missing required fields" });
+        }
+ 
+        await db.run('INSERT INTO expenses (id, description, cost) VALUES (?, ?, ?);', [id, description, cost]);
+        res.status(201).send({ id, description, cost });
+ 
+    } catch (error) {
+ 
+        return res.status(400).send({ error: `Expense could not be created, + ${error}` });
     };
+ 
+ }
+ 
+ 
 
-    expenses.push(newExpense);
-    res.status(201).send(newExpense);
+export async function deleteExpense(req: Request, res: Response, db: Database) {
+    try {
+        // Type casting the request body to the expected format.
+        const { id } = req.body as { id: string };
+ 
+        if (!id) {
+            return res.status(400).send({ error: "Missing required fields" });
+        }
+ 
+        await db.run('DELETE FROM expenses WHERE id = ?', [id]);
+        res.status(200).send({ id });
+ 
+    } catch (error) {
+        return res.status(400).send({ error: `Expense could not be deleted: ${error}` });
+    };
 }
 
-export function deleteExpense(req: Request, res: Response, expenses: Expense[]) {
-    const { id } = req.params;
-    
-    if (!id) {
-        return res.status(400).send({ error: "Missing expense ID" });
-    }
+export async function getExpenses(req: Request, res: Response, db: Database) {
+    try {
+        // Get all expenses from database
+        const expenses: Expense[] = await db.all('SELECT * FROM expenses');
+        res.status(200).send(expenses);
 
-    const expenseIndex = expenses.findIndex(expense => expense.id === id);
-    
-    if (expenseIndex === -1) {
-        return res.status(404).send({ error: "Expense not found" });
-    }
+    } catch (error) {
 
-    expenses.splice(expenseIndex, 1);
-    res.status(200).send({ message: "Expense deleted successfully" });
-}
-
-export function getExpenses(req: Request, res: Response, expenses: Expense[]) {
-    res.status(200).send({ "data": expenses });
+        return res.status(400).send({ error: `Expense could not be created, + ${error}` });
+    };
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,8 @@
-import { Request, Response } from "express";
-import { budget, expenses } from "./constants";
-import { createExpenseEndpoints } from "./expenses/expense-endpoints";
+import { Response } from "express";
 import { createBudgetEndpoints } from "./budget/budget-endpoints";
+import { createExpenseEndpoints } from "./expenses/expense-endpoints";
+import { budget } from "./constants";
+import initDB from "./createTable";
 
 const express = require("express");
 const cors = require("cors");
@@ -14,14 +15,21 @@ app.use(express.json());
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+ console.log(`Server running at http://localhost:${port}`);
 });
 
-// Root endpoint to get test if the server is running
-app.get("/", (req: Request, res: Response) => {
-  res.send({ "data": "Hello, TypeScript Express!" });
-  res.status(200);
-});
+// Initialize the database and start the server
+(async () => {
+ const db = await initDB();
 
-createExpenseEndpoints(app, expenses);
-createBudgetEndpoints(app, budget);
+ // Root endpoint to get test if the server is running
+ app.get("/", (res: Response) => {
+   res.send({ "data": "Hello, TypeScript Express!" });
+   res.status(200);
+ });
+
+ createExpenseEndpoints(app, db);
+
+ createBudgetEndpoints(app, budget);
+})();
+
